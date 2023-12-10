1ad6056369350f1afb899beef247503bc04af233
Samat <xzwqt@mail.ru>
2023-12-10 19:05:59 +0700

    add loader, page not found

---
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -0,0 +1,14 @@
+import { useTranslation } from 'react-i18next';
+
+import { classNames } from 'shared/lib/classNames/classNames';
+import styles from './NotFoundPage.module.scss';
+
+export default function NotFoundPage() {
+  const { t } = useTranslation();
+
+  return (
+    <div className={classNames(styles.NotFoundPage, {}, [])}>
+      {t('Страница не найдена')}
+    </div>
+  );
+}
