652fcf05a9757708cd88f4426b87c827e4d8fbef
Samat <xzwqt@mail.ru>
2024-04-10 17:11:55 +0700

    add documentation for project, update screens, add theme addon for storybook

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -1,6 +1,5 @@
 import { addDecorator } from '@storybook/react';
 import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
-import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
 import { SuspenseDecorator } from '../../src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator';
 import { Theme } from '../../src/shared/const/theme';
@@ -13,9 +12,17 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  layout: 'fullscreen',
+  themes: {
+    default: 'light',
+    list: [
+      { name: 'light', class: ['app', Theme.LIGHT], color: '#fff' },
+      { name: 'dark', class: ['app', Theme.DARK], color: '#000' },
+      { name: 'orange', class: ['app', Theme.ORANGE], color: '#ffb005' },
+    ],
+  },
 };
 
 addDecorator(StyleDecorator);
-addDecorator(ThemeDecorator(Theme.LIGHT));
 addDecorator(RouterDecorator);
 addDecorator(SuspenseDecorator);
